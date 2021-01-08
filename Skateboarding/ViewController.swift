0d2f213fa90bfe2e74a57915e6779c4d13187e97
masayoshi.suzuki <sp1103masa@gmail.com>
2021-01-08 12:55:51 +0900

    Initial Commit

---
+++ Skateboarding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Skateboarding
+//
+//  Created by 鈴木正義 on 2021/01/08.
+//  Copyright © 2021 masayoshi.suzuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
